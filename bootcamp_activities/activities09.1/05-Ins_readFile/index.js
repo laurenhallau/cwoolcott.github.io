45893e2cd828d4accb3631a6963b183502cd0c07
Chris Woolcott <chris.woolcott@quotacy.com>
2019-11-04 21:45:17 -0600

    9.1 solutions

--- bootcamp_activities/activities09.1/05-Ins_readFile/index.js
+++ bootcamp_activities/activities09.1/05-Ins_readFile/index.js
@@ -3,12 +3,10 @@ var fs = require("fs");
 
 // return the contents of 'data.csv' as a string in the variable "data"
 // "utf8" encodes the raw buffer data in human-readable format
-fs.readFile("data.csv", "utf8", function(error, data) {
-
+fs.readFile("data123.csv", "utf8", function(error, data) {
   if (error) {
     return console.log(error);
   }
 
   console.log(data);
-
 });
