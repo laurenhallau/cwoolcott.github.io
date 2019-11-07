9563b62f9628a2749233a7f72a697fb0bf01fa5a
Chris Woolcott <chris.woolcott@quotacy.com>
2019-11-06 21:39:51 -0600

    9.2

--- bootcamp_activities/activities09.2/solved/22-Stu_FunctionalLoops_Solved/map.js
+++ bootcamp_activities/activities09.2/solved/22-Stu_FunctionalLoops_Solved/map.js
@@ -11,15 +11,12 @@ console.log(originalArray);
 
 // 1. A map that triples the originalArray and sets the result equal to a new `tripledArray` array
 
-var tripledArray = originalArray.map(data => data * 3);
+const tripledArray = originalArray.map(data => data * 3);
 
 // 2. A map that takes the originalArray and returns a new array `oddOrEven`
 // containing the text "even" if the number is even and the text "odd" if the number is odd
 // i.e. using this map, the array [3, 1, 2, 4] should give us back ["odd", "odd", "even", "even"]
 
-// Your code here
-
 const oddOrEven = originalArray.map(num => (num % 2 === 0 ? "even" : "odd"));
 
-console.log(oddOrEven);
 // Bonus: Use arrow functions as callbacks!
