986f8f8b8f39c67aab675f41b14d7fcc06f9ca29
Chris Woolcott <chris.woolcott@quotacy.com>
2019-11-09 09:50:46 -0600

    update review

---
+++ bootcamp_activities/activities09.3/0-ChrisReview/inclass.js
@@ -0,0 +1,22 @@
+// ***** Object Destructuring  *****
+
+const client = {
+  name: "Jack Johnson",
+  company: "Jack Company",
+  balance: 150
+};
+
+// ***** Array Destructuring *****
+
+const introduction = ["Hello", "I", "am", "Chris"];
+
+// ***** forEach / map *****
+
+const staff = [
+  { id: 123, name: "Zack Morris" },
+  { id: 534, name: "A.C. Slater" },
+  { id: 567, name: "Lisa Turtle" },
+  { id: 234, name: "Kelly Kapowski" }
+];
+
+// ***** ForEach (Get Just Ids) *****
--- bootcamp_activities/activities09.3/0-ChrisReview/index.js
+++ bootcamp_activities/activities09.3/0-ChrisReview/index.js
@@ -1,36 +1,35 @@
 // ***** Object Destructuring  *****
 
-let client = 
-{
+const client = {
   name: "Jack Johnson",
   company: "Jack Company",
   balance: 150
 };
 
-let {name} = client; // Just Name
+let { name } = client; // Just Name
 
-console.log(name);  
+console.log(name);
 
 // ***** Array Destructuring *****
 
-let introduction = ["Hello", "I" , "am", "Chris"];
-let [greeting,,,firstName] = introduction;
+const introduction = ["Hello", "I", "am", "Chris"];
+let [greeting, , , firstName] = introduction;
 
-console.log(greeting,firstName) //Hello Chris
+console.log(greeting, firstName); //Hello Chris
 
 // ***** forEach / map *****
 
-var staff = [
-  { id: 123, name: 'Zack Morris' },
-  { id: 534, name: 'A.C. Slater' },
-  { id: 567, name: 'Lisa Turtle' },
-  { id: 234, name: 'Kelly Kapowski' }
+const staff = [
+  { id: 123, name: "Zack Morris" },
+  { id: 534, name: "A.C. Slater" },
+  { id: 567, name: "Lisa Turtle" },
+  { id: 234, name: "Kelly Kapowski" }
 ];
 
 // ***** ForEach (Get Just Ids) *****
 
-var employeeIds = [];
-staff.forEach(function(officer){
+const employeeIds = [];
+staff.forEach(function(officer) {
   employeeIds.push(officer.id);
 });
 
@@ -38,14 +37,15 @@ console.log(employeeIds);
 
 // ***** ...OR... Map (Get Just Ids) *****
 
-var employeeIdsAlt = staff.map(function(officer){
-  return officer.id
+const employeeIdsAlt = staff.map(function(officer) {
+  return officer.id;
 });
 
 console.log(employeeIdsAlt);
 
 // ***** Template Literals *****
 
-const string = `${name} Company and ${firstName} assigned to staff #${employeeIdsAlt[0]}`;
+const string = `${name} Company and ${firstName} assigned to staff #${
+  employeeIdsAlt[0]
+}`;
 console.log(string);
-
