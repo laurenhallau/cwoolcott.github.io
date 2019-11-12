8a6c20545e09a10d96e3c719c18b0f9e474e70f9
Chris Woolcott <chris.woolcott@quotacy.com>
2019-11-12 16:58:39 -0600

    hints

--- bootcamp_activities/activities10.2/10-Stu_Pass-Tests/algo.js
+++ bootcamp_activities/activities10.2/10-Stu_Pass-Tests/algo.js
@@ -4,6 +4,10 @@ Algo.prototype.reverse = function(str) {};
 
 Algo.prototype.isPalindrome = function(str) {};
 
-Algo.prototype.capitalize = function(str) {};
+Algo.prototype.capitalize = function(str) {
+  return str.split(" ").map(word => {
+    return word.substring(0, 1).toUpperCase() + word.substring(1);
+  }).join(" ");
+};
 
 module.exports = Algo;
