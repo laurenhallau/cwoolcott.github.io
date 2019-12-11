cbe8b31ed3a1001a962daaf203cbd4d1e537005e
Chris Woolcott <chris.woolcott@quotacy.com>
2019-12-11 16:05:47 -0600

    simplied activities 13.3

--- bootcamp_activities/activities13.3/11-QuotesApp/server.js
+++ bootcamp_activities/activities13.3/11-QuotesApp/server.js
@@ -1,16 +1,19 @@
-var express = require("express");
 var exphbs = require("express-handlebars");
+var express = require("express");
 var mysql = require("mysql");
 
 var app = express();
 
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
---
+++ bootcamp_activities/activities13.3/13-PartyDatabase/server.js
@@ -0,0 +1,13 @@
+var orm = require("./config/orm.js");
+
+// For each of the following select methods, a string argument containing wildcard character ("*")
+// could work in most environments, but some MySQL servers (like MAMP) will return an error.
+
+// Console log all the party_name's.
+
+// Console log all the client_name's.
+
+// Console log all the parties that have a party-type of grown-up.
+
+// Console log all the clients and their parties. (*HINT: HARD USE LEFT JOIN)
+
