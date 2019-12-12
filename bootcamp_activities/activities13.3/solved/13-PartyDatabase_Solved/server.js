fa93b096973fb15faa068f04087d8c829728d970
Chris Woolcott <chris.woolcott@quotacy.com>
2019-12-11 21:45:26 -0600

    13.3 solutions

--- bootcamp_activities/activities13.3/solved/13-PartyDatabase_Solved/server.js
+++ bootcamp_activities/activities13.3/solved/13-PartyDatabase_Solved/server.js
@@ -12,5 +12,5 @@ orm.select("client_name", "clients");
 // Console log all the parties that have a party-type of grown-up.
 orm.selectWhere("parties", "party_type", "grown-up");
 
-// Console log all the clients and their parties. (*HINT: HARD USE LEFT JOIN)
-orm.leftJoin("*", "parties", "clients", "client_id", "id");
+// Console log all the clients and their parties.
+orm.leftJoin(["client_name", "party_name"], "clients", "parties", "id", "client_id");
