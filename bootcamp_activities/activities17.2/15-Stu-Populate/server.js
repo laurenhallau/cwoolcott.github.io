1680fd7972c6ae42e56ffbd153e7edb82b717585
Chris Woolcott <chris.woolcott@quotacy.com>
2020-01-17 22:43:03 -0600

    updates 17.2

--- bootcamp_activities/activities17.2/15-Stu-Populate/server.js
+++ bootcamp_activities/activities17.2/15-Stu-Populate/server.js
@@ -62,6 +62,7 @@ app.get("/populateduser", (req, res) => {
   // Write the query to grab the documents from the User collection,
   // and populate them with any associated Notes.
   // TIP: Check the models out to see how the Notes refers to the User
+  // res.json(err)
 });
 
 // Start the server
